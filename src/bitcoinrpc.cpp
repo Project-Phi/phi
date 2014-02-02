58234fdee38223b343d453ce91c1785ecf071300
Rexota <winkan.32@gmail.com>
2014-02-02 22:20:27 +0200

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 1745 : 745;
+    return GetBoolArg("-testnet", false) ? 17452 : 7452;
 }
 
 Object JSONRPCError(int code, const string& message)
